b166faf08e2c8f5c88ee5a7a2a9058d0758d59c2
ValRobichaux <val.robichaux@gmail.com>
2024-04-16 21:55:48 -0500

    Finished listen and accept methods for TCP

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,48 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) listenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+		}
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("new incoming connection %+v\n", conn)
+}
