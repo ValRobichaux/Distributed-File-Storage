73547f4bae4ffa295787555840564497cdf4d005
ValRobichaux <val.robichaux@gmail.com>
2024-04-21 20:24:54 -0500

    Adding some store tests

--- store.go
+++ store.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+func CASPathTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+
+	return strings.Join(paths, "/")
+}
 
 type PathTransformFunc func(string) string
 
@@ -28,13 +51,17 @@ func NewStore(opts StoreOpts) *Store {
 
 func (s *Store) writeStream(key string, r io.Reader) error {
 	pathName := s.PathTransformFunc(key)
+	fmt.Printf("Path generated %s", pathName)
 
 	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	filename := "somefilename"
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
 
+	filenameBytes := md5.Sum(buf.Bytes())
+	filename := hex.EncodeToString(filenameBytes[:])
 	pathAndFilename := pathName + "/" + filename
 
 	f, err := os.Create(pathAndFilename)
@@ -42,7 +69,7 @@ func (s *Store) writeStream(key string, r io.Reader) error {
 		return err
 	}
 
-	n, err := io.Copy(f, r)
+	n, err := io.Copy(f, buf)
 	if err != nil {
 		return err
 	}
--- store_test.go
+++ store_test.go
@@ -2,17 +2,62 @@ package main
 
 import (
 	"bytes"
+	"os"
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestpicture"
+	pathname := CASPathTransformFunc(key)
+	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathname != expectedPathName {
+		t.Errorf("have %s want %s", pathname, expectedPathName)
+	}
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultpathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
 
 	data := bytes.NewReader([]byte("some jpg bytes"))
 	if err := s.writeStream("myspecialpicture", data); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestStoreWriteStream(t *testing.T) {
+	tests := []struct {
+		key             string
+		expectedFolders int
+	}{
+		{"momsbestpicture", 7},       // Expected number of folders created for the given key
+		{"anotherspecialpicture", 7}, // Another example
+	}
+
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc, // Using CASPathTransformFunc for testing
+	}
+	s := NewStore(opts)
+
+	for _, test := range tests {
+		t.Run(test.key, func(t *testing.T) {
+			defer os.RemoveAll(test.key) // Cleanup after each test case
+
+			data := bytes.NewReader([]byte("some jpg bytes")) // Mocking JPG bytes
+			err := s.writeStream(test.key, data)
+			if err != nil {
+				t.Errorf("Error writing stream: %v", err)
+			}
+
+			// Check if the expected number of folders were created
+			// Here, we'll just check if the top-level folder was created
+			// You might want to expand this to check for all subfolders
+			_, err = os.Stat(test.key)
+			if os.IsNotExist(err) {
+				t.Errorf("Expected folder %s was not created", test.key)
+			}
+		})
 	}
 }
