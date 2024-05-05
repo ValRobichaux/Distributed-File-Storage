1815778e17d79d9f9249a6e625c4b3ffeeff5565
ValRobichaux <val.robichaux@gmail.com>
2024-05-04 22:50:52 -0500

    small testing change

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ run: build
 	@./bin/fs
 
 test: 
-	@go test ./... -v 
+	@go test ./...
--- store_test.go
+++ store_test.go
@@ -10,13 +10,13 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestpicture"
 	pathKey := CASPathTransformFunc(key)
+	expectedFileName := "6804429f74181a63c50c3d81d733a12f14a353ff"
 	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
-	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
 	if pathKey.PathName != expectedPathName {
 		t.Errorf("have %s want %s", pathKey.PathName, expectedPathName)
 	}
-	if pathKey.Filename != expectedOriginalKey {
-		t.Errorf("have %s want %s", pathKey.Filename, expectedOriginalKey)
+	if pathKey.Filename != expectedFileName {
+		t.Errorf("have %s want %s", pathKey.Filename, expectedFileName)
 	}
 }
 
